6b87bdc464aea5f8a5c6843ba7a3fa4841f460da
LucasKnezevich <knezevich.lucas@gmail.com>
2022-11-23 21:02:36 -0800

    Messing around.

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,13 @@
-
+import React, { useState } from "react";
 
 function Home() {
+
+  const [userInput, setUserInput] = useState('');
+
   return (
     <div>
 
-      <h1>Hello</h1>
+      <h1 className="h1">Hello</h1>
 
     </div>
   )
