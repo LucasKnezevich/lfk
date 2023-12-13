dee116a87983b80b8885a6302e225a5445f1f777
Lucas Knezevich <46425585+LucasKnezevich@users.noreply.github.com>
2023-12-13 13:37:54 -0800

    Theme toggle switch added. (#10)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,7 +16,7 @@ const Footer = () => {
           <Mail className="clickable_icon"/>
         </a>
       </div>
-      <p>&copy; Lucas Knezevich &nbsp;|&nbsp; 
+      <p>&copy; Lucas Knezevich &nbsp;|&nbsp;&nbsp; 
         <Link to='/wings' className="wings_link">2024</Link>
       </p>
 
---
+++ src/components/ThemeToggle.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import { Sun, Moon } from "react-feather";
+
+export const ThemeToggle = () => {
+  const [isDarkTheme, setIsDarkTheme] = useState(true);
+
+  const handleToggle = () => {
+    setIsDarkTheme(!isDarkTheme);
+  };
+
+  useEffect(() => {
+    if (isDarkTheme) {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  }, [isDarkTheme]);
+
+  return (
+    <div className="theme-toggle-switch">
+      <label className="theme-label">
+        <input
+          type="checkbox"
+          className="theme-checkbox"
+          checked={isDarkTheme}
+          onChange={handleToggle}
+        />
+        <span className="theme-slider" />
+        {isDarkTheme ? <Moon className="theme-icon"/> : <Sun className="theme-icon"/>}
+        {/* <span className="theme-slider" /> */}
+      </label>
+    </div>
+  );
+};
