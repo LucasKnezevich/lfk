9448d1003ee1c54f7fc04b7e6cfbce25cb3b3c1d
Lucas Knezevich <46425585+LucasKnezevich@users.noreply.github.com>
2024-01-28 11:24:49 -0800

    Repositioned theme switch, small domain description update. (#29)

--- src/components/header_footer/Header.jsx
+++ src/components/header_footer/Header.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import NavBar from './NavBar'
+import ThemeSwitch from './ThemeSwitch'
 
 const Header = () => {
   return (
     <div className="header">
-      <NavBar className="navbar-header"/>
       <div className="header-title">
         <Link to='/' className="home-title-link">Lucas Knezevich</Link>
+      </div>
+      <div className='header-options'>
+        <ThemeSwitch className="nav-theme-switch"/>
+        <NavBar className="navbar-header"/>
       </div>
     </div>
   )
